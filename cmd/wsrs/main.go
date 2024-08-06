9fdd354870ece71e15996971151b5a87ab9ca592
dev-joao <joao.dfss@gmail.com>
2024-08-06 00:30:18 -0300

    subir todos os arquivos iniciais

--- cmd/wsrs/main.go
+++ cmd/wsrs/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"net/http"
 	"os"
+	"os/signal"
+
+	"github.com/rocketseat-education/semana-tech-go-react-server/internal/api"
+	"github.com/rocketseat-education/semana-tech-go-react-server/internal/store/pgstore"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
@@ -24,4 +30,27 @@ func main() {
 		os.Getenv("WSRS_DATABASE_PORT"),
 		os.Getenv("WSRS_DATABASE_NAME"),
 	))
+	if err != nil {
+		panic(err)
+	}
+
+	defer pool.Close()
+
+	if err := pool.Ping(ctx); err != nil {
+		panic(err)
+	}
+
+	handler := api.NewHandler(pgstore.New(pool))
+
+	go func() {
+		if err := http.ListenAndServe(":8080", handler); err != nil {
+			if !errors.Is(err, http.ErrServerClosed) {
+				panic(err)
+			}
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
 }
